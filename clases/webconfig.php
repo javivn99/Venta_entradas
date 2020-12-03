e0423cbe57d8056240976bd1ac34bc169efb19b9
javivn99 <javiyosoyyo@gmail.com>
2020-12-03 20:46:08 +0100

    cambio l aliada

--- clases/webconfig.php
+++ clases/webconfig.php
@@ -1,6 +1,6 @@
 <?php
-//define('DNS','mysql:dbname=conciertos;host=node29137-env-2723241.es-1.axarnet.cloud');  
-define('DNS','mysql:dbname=conciertos;host=localhost');  
+define('DNS','mysql:dbname=conciertos;host=node29137-env-2723241.es-1.axarnet.cloud');  
+//define('DNS','mysql:dbname=conciertos;host=localhost');  
 define('USERNAME','root');  
 define('PASSWORD','ITEyiy14761');
 ?>
