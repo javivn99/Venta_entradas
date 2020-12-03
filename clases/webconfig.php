e634f4b48af4761715d2bd3b1102ec3ad681336a
juanaves <juanaves@gmail.com>
2020-12-03 19:23:49 +0100

    commit 3

--- clases/webconfig.php
+++ clases/webconfig.php
@@ -1,5 +1,5 @@
 <?php
-define('DNS','mysql:dbname=conciertos;host=https://node29125-env-9549929.es-1.axarnet.cloud');  
+define('DNS','mysql:dbname=conciertos;host=node29125-env-9549929.es-1.axarnet.cloud');  
 define('USERNAME','root');  
 define('PASSWORD','TILfgn81490');
 ?>
