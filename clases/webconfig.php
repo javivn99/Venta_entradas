e423dcd30347c9b77c8158497d9806a1b381a4cf
javivn99 <javiyosoyyo@gmail.com>
2020-12-03 20:40:35 +0100

    cambios ahora ya desplegado

--- clases/webconfig.php
+++ clases/webconfig.php
@@ -1,8 +1,8 @@
 <?php
-//define('DNS','mysql:dbname=conciertos;host=node29125-env-9549929.es-1.axarnet.cloud');  
+//define('DNS','mysql:dbname=conciertos;host=node29137-env-2723241.es-1.axarnet.cloud');  
 define('DNS','mysql:dbname=conciertos;host=localhost');  
 define('USERNAME','root');  
-define('PASSWORD','root');
+define('PASSWORD','ITEyiy14761');
 ?>
 
 		
